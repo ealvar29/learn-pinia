74789ab2508627c420ee7d28443f59e17e89dcd2
Eduardo <edalvarez2015@gmail.com>
2022-02-23 12:45:03 -0600

    cleaned files, using store.increment()

--- pinia/src/App.vue
+++ pinia/src/App.vue
@@ -1,20 +1,17 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { useCounterStore } from './stores/counter'
+
+const store = useCounterStore() 
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Learn Pinia" />
     </div>
+    <button @click="store.increment()">{{ store.counter}}</button>
   </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style>
